b4a853d6f651d8e3385bf9736a68bf3d562e936b
Nikolai <nikolai__92@hotmail.com>
2026-06-03 20:17:56 +0200

    add

--- terraform/reusable-workflow-example/main.tf
+++ terraform/reusable-workflow-example/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "rg-simple-deploys-workflow-example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example-2" {
+  name     = "rg-simple-deploys-workflow-example-2"
+  location = "West Europe"
+}
